e5f77a8cf4e728bdcd664727cce73482b2e60975
Ben <shilinchen1010@gmail.com>
2020-04-19 02:41:58 -0700

    Headlines, search updated

--- Final Project/scriptNews.js
+++ Final Project/scriptNews.js
@@ -10,7 +10,7 @@ function initialize() {
     // Hide the loader in the beginning
     let loader = document.getElementById("loader");
     loader.style.display = "none";
-    let headLineT = document.getElementById("headLineTitle");
+    let headLine = document.getElementById("headLineTitle");
     headLine.style.display = "none";
   }
   
@@ -20,12 +20,9 @@ function initialize() {
 async function submit() {
   console.log("In submit!");
 
-  // Set the mouse cursor to hourglass
-  //document.body.style.cursor = "wait";
+  document.body.style.cursor = "wait";
 
-  // Accessing the div that has random value
-//   let random_value_element = document.getElementById("random-value");
-//   random_value_element.innerHTML = "Please wait...";
+  // Get search topic from the user
   let searchTopic = document.getElementById("searchTopic").value;
 
   // Show the loader element (spinning wheels)
@@ -33,11 +30,8 @@ async function submit() {
   loader.style.display = "inline-block";
 
   try {
-    // Get the min/max values from the user
-    let searchTopic = document.getElementById("searchTopic").value;
-    //let searchTopic = document.getElementById("searchTopic").value;
 
-    // Address of your backend
+    // Address of backend
     let request = `http://127.0.0.1:5000/?searchTopic=${searchTopic}`;
     console.log("request: ", request);
 
@@ -45,8 +39,38 @@ async function submit() {
     const data = await axios.get(request);
 
     console.log("data.data: ", JSON.stringify(data.data, null, 2));
-    
-    console.log("Search Topic: ", searchTopic);
+    const url_search = `http://newsapi.org/v2/everything?`+
+                       `q=${searchTopic}&` +
+                       `from=2020-03-19&` +
+                       `sortBy=publishedAt&` +
+                       `apiKey=${apiKey}`;
+
+    const searchNews = (newsdata) => {
+            const articlesDiv = document.querySelector(".articles")
+            newsdata.articles.forEach((article) => {
+                    
+            //Here we create and add html elements to our html file
+                const div = document.createElement("div")
+                div.className = "news"
+                div.innerHTML = `
+                <br>
+                <a href="${article.url}">
+                  <img src="${article.urlToImage}" alt="News Image" style="width:100%">
+                   <div class = "content">
+                    <h1> ${article.title} </h1>
+                    <p> ${article.description} </p>
+                  </div>
+                </a>
+                `
+                articlesDiv.appendChild(div);
+                
+            })
+        }
+        fetch(url_search)
+        .then(response => response.json())
+        .then(searchNews)
+        
+        console.log(url_search);
     
   } catch (error) {
     console.log("error: ", error);
@@ -59,15 +83,17 @@ async function submit() {
   loader.style.display = "none";
 }
 
+// do an other version for TW headline
 async function headLine() {
     console.log("In headLine!");
-  
-    let searchTopic = document.getElementById("searchTopic").value;
   
     // Show the loader element (spinning wheels)
     let loader = document.getElementById("loader");
     loader.style.display = "inline-block";
-    
+    let headline = document.getElementById("headLineTitle");
+    headline.style.display = "block";
+
+    // display Headline News
     try {
         
         const recieveNews = (newsdata) => {
@@ -78,8 +104,15 @@ async function headLine() {
                 const div = document.createElement("div")
                 div.className = "news"
                 div.innerHTML = `
-                <h2>${article.title}</h2>
-                <img src="${article.urlToImage}"/>`
+                <br>
+                <a href="${article.url}">
+                  <img src="${article.urlToImage}" alt="News Image" style="width:100%">
+                   <div class = "content">
+                    <h1> ${article.title} </h1>
+                    <p> ${article.content} </p>
+                  </div>
+                </a>
+                `
                 articlesDiv.appendChild(div);
                 
             })
@@ -99,68 +132,3 @@ async function headLine() {
     // Hide loader animation
     loader.style.display = "none";
   }
-  
-
-//console.log("topic: ", searchTopic);
-
-   
-    
-    // const newsList = document.querySelector('.news-list');
-    
-    // document.getElementById("submit").addEventListener("onclick", retrieve)
-
-    // function retrieve(e) {
-
-    //     if (input.value == '') {
-    //         alert('Input is empty!');
-    //     }
-
-    //     e.preventDefault()
-        
-    //     const apiKey = '504949294bc7419d83beebf27c708229'
-
-    //     let searchTopic = searchTopic;
-
-    //     let url = `http://newsapi.org/v2/everything?q=${searchTopic}&apiKey=${apiKey}`
-
-    //     fetch(url).then((res) => {
-    //         return res.json()
-    //     }).then((data)=> {
-    //         console.log(data);
-    //         data.articles.forEach(article =>{
-    //             let div = document.createElement('div');
-    //             let a = document.createElement('a');
-    //             a.setAttribute('href',article.url);
-    //             a.setAttribute('target', '_blank');
-    //             a.textContent = article.title;
-    //             div.appendChild(a);
-    //             newsList.appendChild(div);
-    //         })
-    //     }).catch((error)=>{
-    //         console.log(error)
-    //     })
-    // }
-    
-
-        //console.log(searchTopic);
-
-// const recieveNews = (newsdata) => {
-//     const articlesDiv = document.querySelector(".articles")
-//     newsdata.articles.forEach((article) => {
-			
-//         //Here we create and add html elements to our html file
-//   const div = document.createElement("div")
-//   div.className = "news"
-//   div.innerHTML = `
-//         <h2>${article.title}</h2>
-//         <img src="${article.urlToImage}"/>`
-//   articlesDiv.appendChild(div)
-        
-// })
-// }
-
-// //Fetch sends a request to the API.
-// //Promise makes it possible to run this in the background. När vi får APIet då går den vidare och skickar tillbaka JSON.
-// fetch(url)
-// .then(response => response.json())
-// .then(recieveNews)
